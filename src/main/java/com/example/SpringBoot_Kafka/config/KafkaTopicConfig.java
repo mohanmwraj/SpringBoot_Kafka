89317f8cea9f99a8a231cc8a0307816fc978d5f9
Mohan Raj <mohanmwraj@gmail.com>
2025-10-19 18:42:36 +0530

    Added Config class for Kafka Topic

---
+++ src/main/java/com/example/SpringBoot_Kafka/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package com.example.SpringBoot_Kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic javaTopic(){
+
+        return TopicBuilder
+                    .name("KafkaTopic")
+                    .build();
+    }
+}
